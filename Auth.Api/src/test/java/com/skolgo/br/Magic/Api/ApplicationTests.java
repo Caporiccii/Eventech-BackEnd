78a381b149cd4232c438d56121eb277f2e066857
Caporiccii <dmarco792@gmail.com.br>
2020-02-29 00:02:50 -0300

    Create a api to login

---
+++ Auth.Api/src/test/java/com/skolgo/br/Magic/Api/ApplicationTests.java
@@ -0,0 +1,17 @@
+package com.skolgo.br.Magic.Api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+	@Test
+	void Should_be_double(){
+
+	}
+
+}
