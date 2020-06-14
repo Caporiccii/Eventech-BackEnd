d19d46bfe986b16c3eeff11ac3e1aa326f78df57
Caporiccii <dmarco792@gmail.com.br>
2020-06-14 00:29:16 -0300

    Fix field companyName

--- eventech-connection-with-outsystems/src/main/java/com/bandtech/eventech/Repository/ICompanyRepository.java
+++ eventech-connection-with-outsystems/src/main/java/com/bandtech/eventech/Repository/ICompanyRepository.java
@@ -13,4 +13,7 @@ public interface ICompanyRepository extends JpaRepository<CompanyJPA,Integer> {
     @Query("select a from CompanyJPA a where a.name in :nome and a.password in :senha")
     List<CompanyJPA> getCompanyandPassword(@Param("nome") String nome,
                                             @Param("senha") String senha);
+    @Query("SELECT name FROM CompanyJPA")
+    String getNameCompany();
+
 }
--- eventech-connection-with-outsystems/src/main/java/com/bandtech/eventech/Service/V2/HistoryService.java
+++ eventech-connection-with-outsystems/src/main/java/com/bandtech/eventech/Service/V2/HistoryService.java
@@ -1,11 +1,16 @@
 package com.bandtech.eventech.Service.V2;
 
+import com.bandtech.eventech.Repository.ICategoryRepository;
+import com.bandtech.eventech.Repository.ICompanyRepository;
 import com.bandtech.eventech.Repository.IEventRepository;
 import com.bandtech.eventech.crosscutting.PilhaObj;
+import com.bandtech.eventech.interfaces.ICategoryOut;
 import com.bandtech.eventech.model.V2.TicketJPA;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 
@@ -13,13 +18,37 @@ public class HistoryService {
 
     @Autowired
     private IEventRepository repository;
+    @Autowired
+    private ICompanyRepository companyRepository;
+    @Autowired
+    private ICategoryRepository categoryRepository;
 
-    private PilhaObj<TicketJPA> history;
+    private PilhaObj<String> history;
     private Integer totalEvent;
-    public String createHistoryEvent(){
-     repository.getNameEvent();
 
+    public String addHistory(){
+        String nameEvent = getNameHistoryEvent();
+
+
+        //for (String a : nameEvent)
         return null;
+    }
+
+    public String getNameHistoryEvent(){
+     String name = repository.getNameEvent();
+     history.push(name);
+     return history.peek();
   }
 
+    public String getNameHistoryCompany(){
+        String nameCompany = companyRepository.getNameCompany();
+        history.push(nameCompany);
+        return history.peek();
+    }
+
+    public String getNameHistoryCategory(){
+        String nameCategory = categoryRepository.getCategoryEvent();
+        history.push(nameCategory);
+        return history.peek();
+    }
 }
