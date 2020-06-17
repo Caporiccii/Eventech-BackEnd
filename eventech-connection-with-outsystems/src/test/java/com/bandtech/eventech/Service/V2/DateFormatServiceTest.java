4e2c1d945f6d33d81ddbf94903deaad627f3a82f
Caporiccii <dmarco792@gmail.com.br>
2020-06-17 00:12:16 -0300

    Create UnitTest classes

---
+++ eventech-connection-with-outsystems/src/test/java/com/bandtech/eventech/Service/V2/DateFormatServiceTest.java
@@ -0,0 +1,12 @@
+package com.bandtech.eventech.Service.V2;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DateFormatServiceTest {
+
+    @Test
+    void formatDate() {
+    }
+}
