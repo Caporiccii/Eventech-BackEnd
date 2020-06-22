81a076b5df32d58400f7960b459e0f3fff9ba2e8
Caporiccii <dmarco792@gmail.com.br>
2020-06-22 17:48:23 -0300

    Test Pathparam

--- eventech-connection-with-outsystems/src/main/java/com/bandtech/eventech/controller/V1/EventController.java
+++ eventech-connection-with-outsystems/src/main/java/com/bandtech/eventech/controller/V1/EventController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.websocket.server.PathParam;
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
@@ -47,7 +48,7 @@ public class EventController {
     }
 
     @GetMapping("/file/{eventId}")
-    public ResponseEntity getFile(@PathVariable("eventId") Long eventId) throws IOException {
+    public ResponseEntity getFile(@PathParam("eventId") Long eventId) throws IOException {
 
         fileService.montaArquivo(eventId);
 
