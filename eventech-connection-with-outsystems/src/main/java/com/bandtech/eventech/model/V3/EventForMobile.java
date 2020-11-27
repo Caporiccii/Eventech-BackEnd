4f2bb7d509947bb969f2fb29cf67c9fe39cbc294
Caporiccii <dmarco792@gmail.com>
2020-11-26 21:20:16 -0300

    test smtp port

--- eventech-connection-with-outsystems/src/main/java/com/bandtech/eventech/model/V3/EventForMobile.java
+++ eventech-connection-with-outsystems/src/main/java/com/bandtech/eventech/model/V3/EventForMobile.java
@@ -1,9 +1,6 @@
 package com.bandtech.eventech.model.V3;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.math.BigDecimal;
 
 @Entity
